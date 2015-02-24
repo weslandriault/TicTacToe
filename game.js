9cdd92bd0acd56fbb8a866cb859141c67e5f13a6
Wes <wesley.landriault@gmail.com>
2015-02-23 19:21:42 -0500

    Got tic-tac-toe game working with if and else statements. Checking for a winner or a tie on the 10th turn.

--- game.js
+++ game.js
@@ -1,26 +1,53 @@
 $(document).ready(function() {
 		var turn = 1;
+		$("#player").text("turn: O");
+
 		$('td').on('click', function() {
+
 			var self = $(this);
 
 			if(turn % 2 == 0){
 				self.text("x")
-				self.addClass('x')
-			
+				// self.addClass('x')
+				var symbol = 'x'
+				$("#player").text("turn: O");
+
 			}
 			else {
 				self.text('o')
-				self.addClass('o')
+				// self.addClass('o')
+				var symbol = 'o'
+				$("#player").text("turn: X");
 			}
 
-			
 			self.off('click');
 			checkWin(symbol);
 			turn++;
 		});
 
 		var checkWin = function(symbol){
-			
+			if (
+				// Ways to win horizontally
+				$("#box1").text() === symbol && $("#box2").text() === symbol && $("#box3").text() === symbol ||
+				$("#box4").text() === symbol && $("#box5").text() === symbol && $("#box6").text() === symbol ||
+				$("#box7").text() === symbol && $("#box8").text() === symbol && $("#box9").text() === symbol ||
+
+				// Ways to win vertically
+				$("#box1").text() === symbol && $("#box4").text() === symbol && $("#box7").text() === symbol ||
+				$("#box2").text() === symbol && $("#box5").text() === symbol && $("#box8").text() === symbol ||
+				$("#box3").text() === symbol && $("#box6").text() === symbol && $("#box9").text() === symbol ||
+				
+				// Ways to win diagonally
+				$("#box1").text() === symbol && $("#box5").text() === symbol && $("#box9").text() === symbol ||
+				$("#box3").text() === symbol && $("#box5").text() === symbol && $("#box7").text() === symbol
+				)
+			 {
+				console.log("Player " + symbol.toUpperCase + " has won!");
+				$('td').off('click')
+			}
+			else if (turn === 10) {
+				alert("Tie Game! No Winner!")
+			}
 		} 
 
 
